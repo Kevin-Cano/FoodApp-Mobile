51d38a2e981b47c0e56ef80ba5698014ab4c8f56
maymeric <aymeric.marec@ynov.com>
2026-06-23 17:27:25 +0200

    reset project

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,5 @@
-// src/app/_layout.tsx
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { useColorScheme } from 'react-native';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <Stack />
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
