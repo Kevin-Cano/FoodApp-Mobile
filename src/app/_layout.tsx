aec33102143902780fab3e43cd6ab09cfd73976e
cakevin <kevin.cano@ynov.com>
2026-06-23 15:21:24 +0200

    Initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,17 @@
+// src/app/_layout.tsx
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { useColorScheme } from 'react-native';
+
+import { AnimatedSplashOverlay } from '@/components/animated-icon';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AnimatedSplashOverlay />
+      <Stack />
+    </ThemeProvider>
+  );
+}
